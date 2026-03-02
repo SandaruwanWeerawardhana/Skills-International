9a26e82a98aaa28d6e2c1e103413f55c5cbcc93e
SandaruwanWeerawardhana <werawardanasw@gmail.com>
2026-03-02 13:13:23 +0530

    Initial commit - project setup

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Skills_International_School_Management_System
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Login());
+        }
+    }
+}

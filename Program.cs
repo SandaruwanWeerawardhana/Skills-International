0a47685ef33312551c2c11cffbfd60ca03f1af72
SandaruwanWeerawardhana <werawardanasw@gmail.com>
2026-03-03 22:29:47 +0530

    Remove unused namespaces from Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Skills_International_School_Management_System

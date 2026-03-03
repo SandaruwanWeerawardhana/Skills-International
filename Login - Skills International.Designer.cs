140c4e7a8ea6734dcfb234806bf7f9407b90e495
SandaruwanWeerawardhana <werawardanasw@gmail.com>
2026-03-03 22:30:20 +0530

    Add UI components and event handlers for Login form

--- Login - Skills International.Designer.cs
+++ Login - Skills International.Designer.cs
@@ -28,20 +28,192 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnclear = new System.Windows.Forms.Button();
+            this.btnexit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.exit = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // Form1
+            // btnclear
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.btnclear.Location = new System.Drawing.Point(524, 854);
+            this.btnclear.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.btnclear.Name = "btnclear";
+            this.btnclear.Size = new System.Drawing.Size(280, 65);
+            this.btnclear.TabIndex = 3;
+            this.btnclear.Text = "Clear";
+            this.btnclear.Click += new System.EventHandler(this.btnclear_Click);
+            // 
+            // btnexit
+            // 
+            this.btnexit.Location = new System.Drawing.Point(867, 626);
+            this.btnexit.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.btnexit.Name = "btnexit";
+            this.btnexit.Size = new System.Drawing.Size(250, 65);
+            this.btnexit.TabIndex = 4;
+            this.btnexit.Text = "Exit";
+            this.btnexit.Click += new System.EventHandler(this.btnexit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Location = new System.Drawing.Point(56, 113);
+            this.label1.Margin = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(259, 35);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Skill International";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // exit
+            // 
+            this.exit.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exit.Location = new System.Drawing.Point(12, 423);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(84, 29);
+            this.exit.TabIndex = 8;
+            this.exit.Text = "Exit";
+            this.exit.UseVisualStyleBackColor = false;
+            this.exit.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // clear
+            // 
+            this.clear.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.clear.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clear.Location = new System.Drawing.Point(21, 166);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(84, 29);
+            this.clear.TabIndex = 9;
+            this.clear.Text = "Clear";
+            this.clear.UseVisualStyleBackColor = false;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(218, 166);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(84, 29);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Login";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.clear);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(33, 177);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(315, 201);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Login";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(100, 94);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(185, 22);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(100, 57);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(185, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 16);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Password";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Username";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ImageLocation = "D:\\Project\\Skills International School Management System\\Asset\\logo.png";
+            this.pictureBox1.Location = new System.Drawing.Point(124, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(122, 69);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(20F, 37F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(384, 450);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(386, 461);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnclear);
+            this.Controls.Add(this.btnexit);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.Name = "Login";
+            this.Text = "Login - Skills International";
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.Button btnclear;
+        private System.Windows.Forms.Button btnexit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button exit;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Login - Skills International.cs
+++ Login - Skills International.cs
@@ -1,20 +1,135 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace Skills_International_School_Management_System
 {
     public partial class Login : Form
     {
+    
+        SqlConnection DB_Con = new SqlConnection(
+            @"Data Source=YOUR_PC_NAME\SQLEXPRESS;" +
+            "Initial Catalog=Student;" +
+            "Integrated Security=True");
+
         public Login()
         {
             InitializeComponent();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+            //txtun.Focus();
+        }
+
+
+
+        private void btnclear_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();  // Username
+            textBox2.Clear();  // Password
+            textBox1.Focus();
+        }
+
+      
+        private void btnexit_Click(object sender, EventArgs e)
+        {
+            DialogResult yesno = MessageBox.Show(
+                "Are You Sure, Do You Really Want to Exit...?",
+                "Exit",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+
+            if (yesno == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+            else if (yesno == DialogResult.No)
+            {
+              
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+   
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult yesno = MessageBox.Show(
+                "Are You Sure, Do You Really Want to Exit...?",
+                "Exit",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+
+            if (yesno == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+            else if (yesno == DialogResult.No)
+            {
+              
+            }
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear(); 
+            textBox2.Clear(); 
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //string query = "SELECT * FROM Logins WHERE username=@user AND password=@pw";
+                //SqlDataAdapter sda = new SqlDataAdapter(query, DB_Con);
+                //sda.SelectCommand.Parameters.AddWithValue("@user", txtun.Text);
+                //sda.SelectCommand.Parameters.AddWithValue("@pw", txtpw.Text);
+
+                //DataTable dt = new DataTable();
+                //sda.Fill(dt);
+
+                //if (dt.Rows.Count == 1)
+                //{
+                // Login success - hide this form and open Registration
+                this.Hide();
+                RegistrationForm rf = new RegistrationForm();
+                rf.Show();
+                //}
+                //else
+                //{
+                //    // Login failed - show error message
+                //    MessageBox.Show(
+                //        "Invalid Login Credentials, Please Check User Name and Password then Try Again",
+                //        "Invalid Login Details",
+                //        MessageBoxButtons.OK,
+                //        MessageBoxIcon.Error);
+                //}
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    "Database connection error: " + ex.Message,
+                    "Connection Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
         }
     }
 }
